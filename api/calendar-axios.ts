ef82fef8bb03ca555ea0afe943fac97bfa60f1d8
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-05-04 02:08:47 +0900

    feat: 로그인 세션이 만료된 경우 로그아웃 처리

--- api/calendar-axios.ts
+++ api/calendar-axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import useAccessToken from '@/hooks/store/useAccessToken';
+import useCalendarEditModal from '@/hooks/store/useCalendarEditModal';
+import useCalendarAddModal from '@/hooks/store/useCalendarAddModal';
 
 const calendarAxios = axios.create({
     baseURL: 'https://www.googleapis.com/calendar/v3',
@@ -17,5 +19,18 @@ calendarAxios.interceptors.request.use((config) => {
     }
     return config;
 });
+
+calendarAxios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            useAccessToken.getState().logout();
+            useCalendarEditModal.getState().setEvent(null);
+            useCalendarAddModal.getState().setJob(null);
+            throw new Error('로그인 세션이 만료됐어요. 다시 로그인 후 시도해 주세요.');
+        }
+        throw error;
+    }
+);
 
 export default calendarAxios;
